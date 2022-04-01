35e0df383536460faef52ac995fe69c7ad6ce72f
Varun <varun.bal.work@gmail.com>
2022-04-01 20:15:13 +0530

    Refactored code

--- apply_filter.py
+++ apply_filter.py
@@ -1,3 +1,4 @@
+import mediapipe as mp
 import cv2
 import math
 import numpy as np
@@ -15,6 +16,44 @@ filter_name = "anonymous"
 # Processing input file
 filename1 = DATA_PATH + f"{filter_name}.png"
 annotation_file = DATA_PATH + f"{filter_name}_annotations.csv"
+
+
+# detect facial landmarks in image
+def getLandmarks(img):
+    mp_face_mesh = mp.solutions.face_mesh
+    selected_keypoint_indices = [127, 93, 58, 136, 150, 149, 176, 148, 152, 377, 400, 378, 379, 365, 288, 323, 356, 70, 63, 105, 66, 55,
+                 285, 296, 334, 293, 300, 168, 6, 195, 4, 64, 60, 94, 290, 439, 33, 160, 158, 173, 153, 144, 398, 385,
+                 387, 466, 373, 380, 61, 40, 39, 0, 269, 270, 291, 321, 405, 17, 181, 91, 78, 81, 13, 311, 306, 402, 14,
+                 178, 162, 54, 67, 10, 297, 284, 389]
+
+    height, width = img.shape[:-1]
+
+    with mp_face_mesh.FaceMesh(max_num_faces=1, static_image_mode=True, min_detection_confidence=0.5) as face_mesh:
+
+        results = face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+        if not results.multi_face_landmarks:
+            print('Face not detected!!!')
+            return 0
+
+        for face_landmarks in results.multi_face_landmarks:
+            values = np.array(face_landmarks.landmark)
+            face_keypnts = np.zeros((len(values), 2))
+
+            for idx,value in enumerate(values):
+                face_keypnts[idx][0] = value.x
+                face_keypnts[idx][1] = value.y
+
+            # Convert normalized points to image coordinates
+            face_keypnts = face_keypnts * (width, height)
+            face_keypnts = face_keypnts.astype('int')
+
+            relevant_keypnts = []
+
+            for i in selected_keypoint_indices:
+                relevant_keypnts.append(face_keypnts[i])
+            return relevant_keypnts
+    return 0
 
 # Read the image and resize it
 img1 = cv2.imread(filename1, cv2.IMREAD_UNCHANGED)
@@ -94,7 +133,7 @@ while True:
 
         # find landmarks after skipping SKIP_FRAMES number of frames
         if count % SKIP_FRAMES == 0:
-            points2 = fbc.getLandmarks(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+            points2 = getLandmarks(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
 
         # convert to float data type
         img1Warped = np.copy(img2)
